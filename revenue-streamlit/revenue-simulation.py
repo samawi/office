380628d5647073c7a89d1a0eb1f74b4ec0f9e886
Samawi Samadikun <samawi.samadikun@gmail.com>
2022-09-07 17:49:46 +0700

    update graph and cleaning raw data

--- revenue-streamlit/revenue-simulation.py
+++ revenue-streamlit/revenue-simulation.py
@@ -12,7 +12,7 @@ from dateutil.relativedelta import relativedelta
 from itertools import islice
 
 # open excel file
-excel_file = "tenancy_list_05SEP2022.xlsx"
+excel_file = "tenancy_list_07SEP2022.xlsx"
 wb = openpyxl.load_workbook(excel_file, data_only=True)
 
 # getting all sheets
@@ -57,6 +57,7 @@ def calculate_revenue(date_start, date_end, df_data, product_type, report_sum):
     df_report_sc_charge = pd.DataFrame(my_reporting_date_range, columns=['date'])       # Service charge revenue
     df_report_sc_charge_rate = pd.DataFrame(my_reporting_date_range, columns=['date'])       # Service charge revenue
     df_report_occupancy = pd.DataFrame(my_reporting_date_range, columns=['date'])       # Occupancy rate
+    df_pid_tenant_name_mapping = pd.DataFrame()
 
     # Set 'date' as the index
     df_report_rental_charge = df_report_rental_charge.set_index('date')
@@ -91,6 +92,7 @@ def calculate_revenue(date_start, date_end, df_data, product_type, report_sum):
         data_area = row['Area']
         data_rental_rate = row['Rental_Rate']
         data_sc_rate = row['SC_Rate']
+        data_tenant_name = row['Tenant']
 
         if (data_area == None) or (substring in str(data_area)):
             data_area = 0.0
@@ -128,6 +130,9 @@ def calculate_revenue(date_start, date_end, df_data, product_type, report_sum):
             str_sep = '-'
             str_temps = [str(str_level), str(str_zone), str(index)]
             str_column_name = str_sep.join(str_temps)
+            dict01 = {'pid':[str_column_name], 'cust_name':[str_column_name + '   ' + data_tenant_name]}
+            df_temp = pd.DataFrame(dict01)
+            df_pid_tenant_name_mapping = pd.concat([df_pid_tenant_name_mapping, df_temp], ignore_index=True)
 
             tenant_date_range = pd.date_range(start=start, end=end, freq='MS')
 
@@ -198,20 +203,27 @@ def calculate_revenue(date_start, date_end, df_data, product_type, report_sum):
     df_sum['Occ'] = df_report_occupancy.resample(report_sum).mean()['sum']
     df_sum['OccPct'] = df_sum['Occ']/area_rentable_office
     df_sum.reset_index(inplace=True)
-    return df_sum, df_report_rental_charge, df_report_sc_charge, df_report_occupancy, df_report_sc_charge_rate
+    return df_sum, df_report_rental_charge, df_report_sc_charge, df_report_occupancy, df_report_sc_charge_rate, df_pid_tenant_name_mapping
 
 df_data = read_worksheet_into_dataframe(sheet_data)
-df_sum, df_report_rental_charge, df_report_sc_charge, df_report_occupancy, df_report_sc_charge_rate = calculate_revenue(date_start, date_end , df_data, 'Office', 'Y')
+df_sum, df_report_rental_charge, df_report_sc_charge, df_report_occupancy, df_report_sc_charge_rate, df_pid_tenant_name_mapping = calculate_revenue(date_start, date_end , df_data, 'Office', 'Y')
 
 # df_sum.to_excel('test1.xlsx')
 
 st.set_page_config(layout = "wide")
+
+# STREAMLIT OUTPUT
+##################
+st.header("The Energy")
+st.code(f"Excel file: {excel_file}")
 
 fig = px.bar(
     df_sum,
     x = 'date',
     y = 'OccPct',
     title = "Occupancy Rates",
+    height=500,
+    text_auto=True,
 )
 fig.update_layout(xaxis_tickangle = -45)
 st.plotly_chart(fig, use_container_width = True)
@@ -222,6 +234,7 @@ fig = px.bar(
     y = ["Rental", "SC"],
     title = "Revenue",
     labels = {'Rental':'Rp'},
+    height=500,
 )
 fig.update_layout(xaxis_tickangle = -45)
 st.plotly_chart(fig, use_container_width = True)
@@ -239,20 +252,24 @@ format_mapping = {
 df_sum_styled = df_sum.style.format(format_mapping)
 st.table(df_sum_styled)
 
+# st.table(df_pid_tenant_name_mapping)
+
 # B/W Heatmap of occupancy
 df = df_report_occupancy.drop(['sum'], axis = 1)
+new_col_names = df_pid_tenant_name_mapping['cust_name'].tolist()
+# new_col_names = ['date'] + new_col_names
+df.columns = new_col_names
 df.mask(df > 0, 1, inplace =True)
-
-fig4 = px.imshow(df.loc['2023'].iloc[:,:], color_continuous_scale="gray")
+fig4 = px.imshow(df.loc[:].iloc[:,:], color_continuous_scale="gray", height=2000)
 fig4.update_traces(xgap = 1, ygap = 1)
 st.plotly_chart(fig4, use_container_width = True)
 
 # Heatmap chart
-df = df_report_rental_charge.loc['2030']
+df = df_report_rental_charge.loc[:]
 df = df.drop(['sum'], axis = 1)
 df = df.T
 fig2 = px.density_heatmap(df)
-st.plotly_chart(fig2, use_container_width = True)
+# st.plotly_chart(fig2, use_container_width = True)
 
 fig3 = px.colors.qualitative.swatches()
-st.plotly_chart(fig3)
+# st.plotly_chart(fig3)
